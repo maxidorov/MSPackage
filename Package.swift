fafb0c866cbb70ada5f470881a064e06714c2424
Maxim Sidorov <s_maxim_v@bk.ru>
2022-09-05 23:51:11 +0300

    specify path

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MSPackage",
-            targets: ["MSPackage"]),
+            targets: ["MSPackage"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +21,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MSPackage",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/"
+        ),
         .testTarget(
             name: "MSPackageTests",
-            dependencies: ["MSPackage"]),
+            dependencies: ["MSPackage"]
+        ),
     ]
 )
