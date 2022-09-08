e645de59bcc42ba20cbb352ddc08d6bdac544f03
Maxim Sidorov <s_maxim_v@bk.ru>
2022-09-08 21:41:59 +0300

    make Utils.swift public

--- Sources/Utils.swift
+++ Sources/Utils.swift
@@ -5,16 +5,16 @@
 //  Created by MSP on 08.09.2022.
 //
 
-func modify<T>(_ a: inout T, _ f: (inout T) -> Void) {
+public func modify<T>(_ a: inout T, _ f: (inout T) -> Void) {
   f(&a)
 }
 
-func modified<T>(_ a: T, _ f: (inout T) -> Void) -> T {
+public func modified<T>(_ a: T, _ f: (inout T) -> Void) -> T {
   var a = a
   f(&a)
   return a
 }
 
-func mapModified<T>(_ a: [T], _ f: (inout T) -> Void) -> [T] {
+public func mapModified<T>(_ a: [T], _ f: (inout T) -> Void) -> [T] {
   a.map { modified($0, f) }
 }
