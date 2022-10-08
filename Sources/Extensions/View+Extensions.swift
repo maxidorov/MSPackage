6c73797a62c56fe1b43acea901462a99e1e9e2f2
Maxim Sidorov <s_maxim_v@bk.ru>
2022-10-08 18:57:55 +0400

    add func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View

--- Sources/Extensions/View+Extensions.swift
+++ Sources/Extensions/View+Extensions.swift
@@ -33,6 +33,13 @@ public extension View {
     )
   }
 
+  func cornerRadius(
+    _ radius: CGFloat,
+    corners: UIRectCorner
+  ) -> some View {
+    clipShape(RoundedCorner(radius: radius, corners: corners))
+  }
+
   func asButton(action: @escaping Action) -> some View {
     Button(action: action, label: { self })
       .buttonStyle(.plain)
@@ -68,6 +75,20 @@ public extension View {
         content: { content(store) }
       )
     }
+  }
+}
+
+struct RoundedCorner: Shape {
+  var radius: CGFloat = .infinity
+  var corners: UIRectCorner = .allCorners
+
+  public func path(in rect: CGRect) -> Path {
+    let path = UIBezierPath(
+      roundedRect: rect,
+      byRoundingCorners: corners,
+      cornerRadii: CGSize(width: radius, height: radius)
+    )
+    return Path(path.cgPath)
   }
 }
 
